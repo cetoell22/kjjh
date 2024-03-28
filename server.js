221a9fc6c84c283faee77b7856ce8bdab481e172
cetoell22 <107325637+cetoell22@users.noreply.github.com>
2024-03-28 14:09:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,14 @@
-var express = require('express'),
-    request = require('request'),
-    bodyParser = require('body-parser'),
-    app = express();
-
-var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
-console.log('Using limit: ', myLimit);
-
-app.use(bodyParser.json({limit: myLimit}));
-
-app.all('*', function (req, res, next) {
-
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-
-    if (req.method === 'OPTIONS') {
-        // CORS Preflight
-        res.send();
-    } else {
-        var targetURL = req.header('Target-URL');
-        if (!targetURL) {
-            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
-            return;
-        }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
-            function (error, response, body) {
-                if (error) {
-                    console.error('error: ' + response.statusCode)
-                }
-//                console.log(body);
-            }).pipe(res);
-    }
-});
-
-app.set('port', process.env.PORT || 3000);
-
-app.listen(app.get('port'), function () {
-    console.log('Proxy server listening on port ' + app.get('port'));
+// index.js
+addEventListener("fetch", event => {
+  let url = new URL(event.request.url);
+  url.hostname = "d2wpgkrer0gjtw.cloudfront.net"; //ubah sesuai kebutuhan
+  let request = new Request(url, event.request);
+  event.respondWith(
+    fetch(request, {
+      headers: {
+        'Referer': '', //ubah sesuai kebutuhan
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36' //ubah sesuai kebutuhan dengan UA Anda
+      }
+    })
+  );
 });
